1f66ebf4465d89cb7238c5405009c0529260baf4
eliep <eprudhomme@gmail.com>
2020-10-16 16:49:06 +0200

    Update setup classifiers and long description

--- setup.py
+++ setup.py
@@ -1,14 +1,24 @@
 import setuptools
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setuptools.setup(
     name='i3-layouts',
     version='0.5.0',
-    description='More dynamics layouts for i3wm',
+    description='Dynamics layouts for i3wm',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url='http://github.com/eliep/i3-layouts',
     author='Elie',
     author_email='eprudhomme@gmail.com',
     license='MIT',
     packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: POSIX :: Linux",
+    ],
     install_requires=[
       'i3ipc'
     ],

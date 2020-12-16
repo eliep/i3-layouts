c3096bbd73db95109452cf777ae895a04c655b10
eliep <eprudhomme@gmail.com>
2020-12-16 23:08:36 +0100

    Bump to version v0.11.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='i3-layouts',
-    version='0.11.0',
+    version='0.11.1',
     description='Dynamics layouts for i3wm',
     long_description=long_description,
     long_description_content_type="text/markdown",
